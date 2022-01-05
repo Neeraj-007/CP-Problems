0f4377531d80ed2fd924af5e3ffc040492e6a57d
Neeraj Singh <89382164+Neeraj-007@users.noreply.github.com>
2022-01-05 23:26:02 +0530

    https://www.codechef.com/START21C/problems/DEVSPORTS

---
+++ https:/www.codechef.com/START21C/problems/DEVSPORTSDEVSPORTS.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+int main(){
+
+    int n;
+    cin>>n;
+    while (n--){
+
+        int z,y,a,b,c;
+        cin>>z>>y>>a>>b>>c;
+        
+        if(z>=a+b+c+y)cout<<"YES"<<endl;
+        else cout<<"NO"<< endl;
+
+
+
+    }
+
+
+    return 0;
+}
