524834fbec48d603b6910a23dcee36572243fb5d
Neeraj Singh <89382164+Neeraj-007@users.noreply.github.com>
2022-01-06 02:31:01 +0530

    Update and rename ANDORUNI.cpp to And_or_Union.cpp

---
+++ And_or_Union.cpp
@@ -0,0 +1,38 @@
+// O(N logN)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+
+        int n, res=0;
+         cin>>n;
+        int arr[n+1];
+        for(int i=1;i<=n;i++)cin>>arr[i];
+
+        for(int i=0;i<32;i++){
+            int one=0;
+            for(int j=1;j<=n;j++){
+                    if(arr[j]&(1<<i))
+                    {   one++; 
+                        if(one>=2) break;}
+                    }
+
+            if( one>=2){
+                //cout<<one<<(1<<i)<<" "<< i<<endl;
+                res=res+(1<<i);
+
+            }
+
+            }
+         cout<<res<<endl;
+
+            }
+
+
+return 0;
+}
